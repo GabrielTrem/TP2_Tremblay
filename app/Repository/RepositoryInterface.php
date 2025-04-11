b30e45c70e7bc3b83db025c6a5e4dff34c1464a3
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-11 08:04:16 -0400

    added missing crud methods to RepositoryInterface

--- app/Repository/RepositoryInterface.php
+++ app/Repository/RepositoryInterface.php
@@ -3,9 +3,10 @@ namespace App\Repository;
 use Illuminate\Database\Eloquent\Model;
 interface RepositoryInterface
 {
-    public function create(array $attributes): Model;
-    public function getById(int $id): ?Model;
+    public function create(array $content);
+    public function getById(int $id);
     public function getAll(int $perPage = 0);
+    public function update($id, array $content);
     public function delete(int $id);
 }
 ?>
