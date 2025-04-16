4ad206dcc3b494d968a9b6bfb9425d3ab85c36bf
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-16 08:09:55 -0400

    Added middleware to the routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\OneCriticPerFilmMiddleware;
 
 Route::middleware('throttle:60,1')->group( function(){ 
-    #Route::post('/films', 'App\Http\Controllers\FilmController@store');
-    #Route::put('/films/{id}', 'App\Http\Controllers\FilmController@update');
-    #Route::delete('/films/{id}', 'App\Http\Controllers\FilmController@destroy');
+    Route::middleware('auth:sanctum')->group( function(){ 
+        Route::middleware(AdminMiddleware::class)->group( function(){ 
+            #Route::post('/films', 'App\Http\Controllers\FilmController@store');
+            #Route::put('/films/{id}', 'App\Http\Controllers\FilmController@update');
+            #Route::delete('/films/{id}', 'App\Http\Controllers\FilmController@destroy');
+        });
+        #Route::post('/critic', 'App\Http\Controllers\CriticController@store')->middleware(OneCriticPerFilmMiddleware::class);
+        #Route::get('/user', 'App\Http\Controllers\UserController@show');
+        #Route::patch('/user', 'App\Http\Controllers\UserController@update');
+    });
     
-    #Route::post('/critic', 'App\Http\Controllers\CriticController@store');
-    
-    #Route::get('/user', 'App\Http\Controllers\UserController@show');
-    #Route::patch('/user', 'App\Http\Controllers\UserController@update');
 });
 
 Route::middleware('throttle:5,1')->group( function(){
