6de050fc708748edf413835aadefc169f186293a
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-16 08:48:51 -0400

    finished route 5 with show function of UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
-use App\Http\Resources\LanguageResource;
 use App\Models\User;
-use App\Models\Language;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-   
+    private UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository){
+        $this->userRepository = $userRepository;
+    }
+
+    public function show(string $id)
+    {
+        try
+        {
+            $user = Auth::user();
+            return (new FilmResource($this->userRepository->getById($user->id)))->response()->setStatusCode(OK);        
+        }
+        catch(QueryException $ex)
+        {
+            abort(NOT_FOUND, "User Not found");
+        }
+        catch(Exception $ex)
+        {
+            abort(SERVER_ERROR, $ex->getMessage());
+        }
+    }
 }
