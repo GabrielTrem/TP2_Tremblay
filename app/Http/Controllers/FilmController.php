6bd264b606d227ae0d6c31d437b6b79880faefe8
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-19 14:06:20 -0400

    Swagger doc for route Create film finished

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -17,7 +17,6 @@ class FilmController extends Controller
         $this->filmRepository = $filmRepository;
     }
 
-    //For test
     public function index(){
         try{
             return $this->filmRepository->getAll();
@@ -27,6 +26,48 @@ class FilmController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/films",
+     *     tags={"Films"},
+     *     summary="Create a new film",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title", "release_year", "length", "description", "rating", "language_id", "special_features", "image"},
+     *             @OA\Property(property="title", type="string", maxLength=50),
+     *             @OA\Property(property="release_year", type="integer"),
+     *             @OA\Property(property="length", type="integer"),
+     *             @OA\Property(property="description", type="string"),
+     *             @OA\Property(property="rating", type="string", maxLength=5),
+     *             @OA\Property(property="language_id", type="integer"),
+     *             @OA\Property(property="special_features", type="string", maxLength=200),
+     *             @OA\Property(property="image", type="string", maxLength=40)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Successful"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Language Not Found"
+     *     ),
+     *     @OA\Response(
+     *         response=422, 
+     *         description="Invalid data"
+     *     ),
+     *     @OA\Response(
+     *         response=429,
+     *         description="Too Many Requests"
+     *     )
+     * )
+     */
     public function store(CreateFilmRequest $request)
     {
         try
