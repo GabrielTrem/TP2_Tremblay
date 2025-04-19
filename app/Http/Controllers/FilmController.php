fd1fce1d85bb8965ebf66ff610b175fb8737a7d9
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-19 14:09:52 -0400

    Swagger doc for route update film finished

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -81,6 +81,55 @@ class FilmController extends Controller
         }  
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/films",
+     *     tags={"Films"},
+     *     summary="Update an existing film",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="film_id",
+     *         description="film id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title", "release_year", "length", "description", "rating", "language_id", "special_features", "image"},
+     *             @OA\Property(property="title", type="string", maxLength=50),
+     *             @OA\Property(property="release_year", type="integer"),
+     *             @OA\Property(property="length", type="integer"),
+     *             @OA\Property(property="description", type="string"),
+     *             @OA\Property(property="rating", type="string", maxLength=5),
+     *             @OA\Property(property="language_id", type="integer"),
+     *             @OA\Property(property="special_features", type="string", maxLength=200),
+     *             @OA\Property(property="image", type="string", maxLength=40)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Successful"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Language Not Found"
+     *     ),
+     *     @OA\Response(
+     *         response=422, 
+     *         description="Invalid data"
+     *     ),
+     *     @OA\Response(
+     *         response=429,
+     *         description="Too Many Requests"
+     *     )
+     * )
+     */
     public function update(UpdateFilmRequest $request, string $id)
     {
         try{
