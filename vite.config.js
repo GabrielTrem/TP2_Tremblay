1de8cf383bc03828bbc1b802d52bd5644a374380
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-04-20 19:29:27 -0400

    changed base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/build/',
+    base: '/public/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
